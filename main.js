781d1810f4647f98050268c905d2a4df17987701
ChauBui00 <c.bui3@share.epsb.ca>
2023-01-17 10:40:21 -0700

    Update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let cnv = document.getElementById("myCanvas");
 let ctx = cnv.getContext("2d");
+let img = document.getElementById("cloud-img");
 cnv.height = 400;
 cnv.width = 400;
 
@@ -9,7 +10,11 @@ ctx.fillRect(0, 0, 400, 300);
 ctx.fillStyle = "green";
 ctx.fillRect(0, 300, 400, 300);
 
-ctx.fill();
+ctx.lineWidth = 5;
 ctx.fillStyle = "red";
+ctx.beginPath();
+ctx.arc(200, 300, 20, Math.PI, 0);
+ctx.fill();
 
-ctx.arc(200, 200, 50, 0, Math.PI, 0);
+ctx.drawImage(img, 120, 90);
+ctx.drawImage(img, 160, 70);
